09e8a7de0eed0265f51eb7df7810f5c6e6e87e2a
tifoputra <tifo.putra@gdn-commerce.com>
2023-06-17 23:39:09 +0700

    final commit

---
+++ SwiftCodeGenerator/SampleScript.swift
@@ -0,0 +1,16 @@
+//
+//  SampleScript.swift
+//  SwiftCodeGenerator
+//
+//  Created by Tifo Audi Alif Putra on 17/06/23.
+//
+
+import Foundation
+
+@main
+struct SampleScript {
+    
+    static func main() {
+        print("execute script! ")
+    }
+}
